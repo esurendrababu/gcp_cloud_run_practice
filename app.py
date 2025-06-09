5dbd3368c34753d794e7de6e482a571d9cb430bf
ETUKURI SURENDRA BABU <112386357+esurendrababu@users.noreply.github.com>
2025-06-09 09:53:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-# app.py
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Cloud Run!"
+    return "✅ Hello from Cloud Run — Flask is working!"
+
+if __name__ == "__main__":
+    # Cloud Run provides PORT as env variable
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
